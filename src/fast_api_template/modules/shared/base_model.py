956110410925a798392e9447f36bba9cdbebc2e0
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-05-30 22:59:15 -0300

    FIX: fix uuid field

--- src/fast_api_template/modules/shared/base_model.py
+++ src/fast_api_template/modules/shared/base_model.py
@@ -1,12 +1,15 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime, timezone
-import uuid
+from uuid import UUID, uuid4
 
 
 class BaseModel(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
-    uuid: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    uuid: UUID = Field(
+        default_factory=uuid4,
+        nullable=False,
+    )
     created_at: Optional[datetime] = Field(
         default_factory=lambda: datetime.now(timezone.utc), nullable=False
     )
