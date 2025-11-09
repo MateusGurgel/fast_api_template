07422cfbf353f9187e8b66e0c5b799c02b7f01ab
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-11-09 02:51:46 -0300

    feat: integrate `SubscriptionPlanRepository` into checkout process and update UUID handling in repository contract

--- src/fast_api_template/modules/stripe/v1/features/create_checkout/create_checkout_injection.py
+++ src/fast_api_template/modules/stripe/v1/features/create_checkout/create_checkout_injection.py
@@ -1,6 +1,10 @@
 from fastapi import Depends
 
 from fast_api_template.core.infra.env import env
+from fast_api_template.modules.subscription_plan.repository.subscription_plan_repository import \
+    SubscriptionPlanRepository
+from fast_api_template.modules.subscription_plan.repository.subscription_plan_repository_injection import \
+    InjectSubscriptionPlanRepository
 from fast_api_template.modules.user.repository.user_repository import UserRepository
 from fast_api_template.modules.user.repository.user_repository_injection import InjectUserRepository
 from .create_checkout_use_case import (
@@ -8,11 +12,12 @@ from .create_checkout_use_case import (
 )
 
 
-def inject_create_checkout_use_case(user_repository: UserRepository = InjectUserRepository) -> CreateCheckoutUseCase:
+def inject_create_checkout_use_case(user_repository: UserRepository = InjectUserRepository, subscription_plan_repository: SubscriptionPlanRepository = InjectSubscriptionPlanRepository) -> CreateCheckoutUseCase:
     return CreateCheckoutUseCase(
         user_repository=user_repository,
         stripe_failure_url=env.stripe_failure_url,
         stripe_success_url=env.stripe_success_url,
+        subscription_plan_repository=subscription_plan_repository
     )
 
 
