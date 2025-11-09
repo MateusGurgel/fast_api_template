810eb492e3ec969f2720026cab64d008146a80b6
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-11-09 00:47:32 -0300

    feat: inject UserRepository into Stripe webhook use case

--- src/fast_api_template/modules/stripe/v1/features/webhook/webhook_injection.py
+++ src/fast_api_template/modules/stripe/v1/features/webhook/webhook_injection.py
@@ -1,13 +1,17 @@
 from fastapi import Depends
 
 from fast_api_template.core.infra.env import env
+from fast_api_template.modules.user.repository.user_repository import UserRepository
+from fast_api_template.modules.user.repository.user_repository_injection import InjectUserRepository
 from .webhook_use_case import (
     WebhookUseCase,
 )
 
 
-def inject_webhook_use_case() -> WebhookUseCase:
-    return WebhookUseCase(env.stripe_webhook_secret)
+def inject_webhook_use_case(user_repository: UserRepository = InjectUserRepository) -> WebhookUseCase:
+    return WebhookUseCase(
+        stripe_webhook_secret=env.stripe_webhook_secret,
+        user_repository=user_repository)
 
 
 InjectWebhookUseCase = Depends(inject_webhook_use_case)
