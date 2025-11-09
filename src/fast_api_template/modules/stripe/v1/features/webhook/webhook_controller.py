58c82f77de90cee2ba7c0614315b9aaee60f97bb
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-11-09 01:09:35 -0300

    feat: add signature validation in Stripe webhook controller with HTTP 400 error handling

--- src/fast_api_template/modules/stripe/v1/features/webhook/webhook_controller.py
+++ src/fast_api_template/modules/stripe/v1/features/webhook/webhook_controller.py
@@ -8,13 +8,20 @@ from .webhook_injection import (
 from .webhook_use_case import (
     WebhookUseCase,
 )
-from fastapi import Request
+from fastapi import Request, HTTPException
+
 
 async def webhook_controller(
     request: Request, webhook_use_case: WebhookUseCase = InjectWebhookUseCase
 ) -> WebhookResponseDTO:
+
+    signature = request.headers.get('stripe-signature')
+
+    if not signature:
+        raise HTTPException(status_code=400, detail="Missing signature header")
+
     result = webhook_use_case.handle(WebhookDTO(
-        signature=request.headers.get('stripe-signature'),
+        signature=signature,
         body=await request.body(),
     ))
     return await result
