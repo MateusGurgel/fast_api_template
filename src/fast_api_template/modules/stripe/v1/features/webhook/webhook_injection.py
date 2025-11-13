7d0b3e5a1fb9fcd63cb56abdd32d7b80b02b9bc6
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-11-13 13:08:01 -0300

    feat: inject `SubscriptionPlanRepository` into `WebhookUseCase` for improved subscription plan handling

--- src/fast_api_template/modules/stripe/v1/features/webhook/webhook_injection.py
+++ src/fast_api_template/modules/stripe/v1/features/webhook/webhook_injection.py
@@ -1,6 +1,10 @@
 from fastapi import Depends
 
 from fast_api_template.core.infra.env import env
+from fast_api_template.modules.subscription_plan.repository.subscription_plan_repository import \
+    SubscriptionPlanRepository
+from fast_api_template.modules.subscription_plan.repository.subscription_plan_repository_injection import \
+    InjectSubscriptionPlanRepository
 from fast_api_template.modules.user.repository.user_repository import UserRepository
 from fast_api_template.modules.user.repository.user_repository_injection import InjectUserRepository
 from .webhook_use_case import (
@@ -8,10 +12,13 @@ from .webhook_use_case import (
 )
 
 
-def inject_webhook_use_case(user_repository: UserRepository = InjectUserRepository) -> WebhookUseCase:
+def inject_webhook_use_case(
+        user_repository: UserRepository = InjectUserRepository, subscription_plan_repository: SubscriptionPlanRepository = InjectSubscriptionPlanRepository) -> WebhookUseCase:
     return WebhookUseCase(
         stripe_webhook_secret=env.stripe_webhook_secret,
-        user_repository=user_repository)
+        user_repository=user_repository,
+        subscription_plan_repository=subscription_plan_repository
+    )
 
 
 InjectWebhookUseCase = Depends(inject_webhook_use_case)
